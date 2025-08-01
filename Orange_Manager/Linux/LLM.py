fca75871ff4eff2b319074b9fc58eec87261b538
Viktor Ilić <vikiilitch@outlook.com>
2025-08-01 15:54:57 +0200

    Update LLM.py

--- Orange_Manager/Linux/LLM.py
+++ Orange_Manager/Linux/LLM.py
@@ -1,9 +1,9 @@
-import g4f
+import g4f, g4f.Provider
 
 class LargeLanguageModel:
     def ResponseFromAI(self, prompt):
         self.user_query = [{f"role": f"user", f"content": prompt}]
         
-        self.response = g4f.ChatCompletion.create(model=f"gpt-4o", messages=self.user_query)
+        self.response = g4f.ChatCompletion.create(model=f"gpt-4o-mini", provider=g4f.Provider.Chatai, messages=self.user_query)
 
         return self.response
